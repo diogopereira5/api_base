7680295dc43ccd89054aacb5cef46e9bea9dbf80
unknown <diogopereira5.ds@gmail.com>
2019-12-19 18:55:03 -0300

    migrate add_column_users_avatar

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -19,9 +20,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res)=> {
-    return res.json({ ok: true});
-});
+//upload de arquivos
+routes.post('/files', upload.single('file'), FileController.store);
 
 
 export default routes;
