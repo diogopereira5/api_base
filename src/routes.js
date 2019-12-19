29c26d53559769bade893df210d4d6684fe30b0e
unknown <diogopereira5.ds@gmail.com>
2019-12-19 17:09:10 -0300

    lib multer

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 
+import multerConfig from './config/multer';
+
 const routes = new Router();
+const upload = multer(multerConfig);
 
 //minhas rotas
 routes.post('/users', UserController.store);
@@ -14,6 +18,10 @@ routes.post('/sessions', SessionController.store);
 //middlewares auth
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res)=> {
+    return res.json({ ok: true});
+});
 
 
 export default routes;
